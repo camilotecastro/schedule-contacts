a66bb0720c78a780496aa4efcbe035bc5053ae14
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-16 00:17:43 -0500

    Initial commit

---
+++ src/main/java/com/camilo/schedulecontacts/ScheduleContactsApplication.java
@@ -0,0 +1,13 @@
+package com.camilo.schedulecontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleContactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/camilo/schedulecontacts/ScheduleContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camilo.schedulecontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
